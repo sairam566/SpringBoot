9651c4568a907bb63de9c8f35084a464dae0c66b
sairam566 <sairam1431995@gmail.com>
2024-03-19 22:20:54 +0530

    Files

---
+++ 05_Spring_Boot_Gradle/app/src/main/java/com/tsr/App.java
@@ -0,0 +1,17 @@
+package com.tsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.tsr.bean.Car;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(App.class, args);
+		Car car = context.getBean(Car.class);
+		car.start();
+	}
+}
---
+++ 05_Spring_Boot_Gradle/app/src/main/java/com/tsr/bean/Car.java
@@ -0,0 +1,10 @@
+package com.tsr.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+	public void start() {
+		System.out.println("Car Started");
+	}
+}
