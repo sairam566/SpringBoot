0b7b0201798149c97313e275bceebc5ebb341d32
sairam566 <sairam1431995@gmail.com>
2024-03-23 20:19:59 +0530

    Executable JAR

---
+++ 06_Executable_Jar/src/com/tsr/Main.java
@@ -0,0 +1,10 @@
+package com.tsr;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+	private static final Logger LOGGER = Logger.getLogger(Main.class);
+	public static void main(String[] args) {
+		LOGGER.debug("Message from Main class");
+	}
+}
