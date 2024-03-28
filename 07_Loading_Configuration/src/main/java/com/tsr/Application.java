da572c6fe869f9b4d36a63633412401d191a2973
sairam566 <sairam1431995@gmail.com>
2024-03-28 10:45:55 +0530

    Main Application

---
+++ 07_Loading_Configuration/src/main/java/com/tsr/Application.java
@@ -0,0 +1,16 @@
+package com.tsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		Student stu = context.getBean(Student.class);
+		System.out.println(stu);
+	}
+
+}
