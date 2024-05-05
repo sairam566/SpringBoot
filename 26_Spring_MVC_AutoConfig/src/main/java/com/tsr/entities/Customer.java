73ee0693a453fad66d86d8616e9e3e76ddaa25ed
sairam566 <sairam1431995@gmail.com>
2024-05-05 12:48:17 +0530

    Entity classes

---
+++ 26_Spring_MVC_AutoConfig/src/main/java/com/tsr/entities/Customer.java
@@ -0,0 +1,71 @@
+package com.tsr.entities;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "customerno")
+	protected int customerNo;
+	
+	@Column(name = "name")
+	protected String name;
+	
+	@Column(name = "dob")
+	protected Date dob;
+	
+	@Column(name = "gender")
+	protected String gender;
+
+	public Customer() {
+		super();
+	}
+
+	public int getCustomerNo() {
+		return customerNo;
+	}
+
+	public void setCustomerNo(int customerNo) {
+		this.customerNo = customerNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerNo=" + customerNo + ", name=" + name + ", dob=" + dob + ", gender=" + gender + "]";
+	}
+
+}
