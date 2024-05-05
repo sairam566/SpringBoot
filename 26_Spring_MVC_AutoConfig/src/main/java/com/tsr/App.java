3b7f3ada624a3d1227e703b552ae42f90774a40e
sairam566 <sairam1431995@gmail.com>
2024-05-05 12:49:01 +0530

    Main class

---
+++ 26_Spring_MVC_AutoConfig/src/main/java/com/tsr/App.java
@@ -0,0 +1,13 @@
+package com.tsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(App.class, args);
+	}
+}
